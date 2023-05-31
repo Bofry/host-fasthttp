82d12b98e4b6e03da12e9415e615a3f28a261555
Wilhelm Liao <williaml.twcp@gmail.com>
2023-05-31 14:46:20 +0800

    REFACTOR: encapsulate extractSpan()

--- internal/tracingutil/requestCtxSpanExtractor.go
+++ internal/tracingutil/requestCtxSpanExtractor.go
@@ -3,6 +3,7 @@ package tracingutil
 import (
 	"context"
 
+	"github.com/Bofry/host-fasthttp/internal/requestutil"
 	"github.com/Bofry/trace"
 	http "github.com/valyala/fasthttp"
 )
@@ -16,7 +17,16 @@ type RequestCtxSpanExtractor int
 // Extract implements trace.SpanExtractor
 func (RequestCtxSpanExtractor) Extract(ctx context.Context) *trace.SeveritySpan {
 	if v, ok := ctx.(*http.RequestCtx); ok {
-		return ExtractSpan(v)
+		return extractSpan(v)
+	}
+	return nil
+}
+
+func extractSpan(ctx *http.RequestCtx) *trace.SeveritySpan {
+	obj := requestutil.ExtractSpan(ctx)
+	v, ok := obj.(*trace.SeveritySpan)
+	if ok {
+		return v
 	}
 	return nil
 }
