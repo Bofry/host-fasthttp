f1645f2955b0b86067da209f1b0f0f9b43162f19
Wilhelm Liao <williaml.twcp@gmail.com>
2023-10-27 17:23:37 +0800

    WIP: handle MessageClient.Close() and error

--- app/websocket/messageClient.go
+++ app/websocket/messageClient.go
@@ -91,9 +91,6 @@ func (client *MessageClient) Stop() {
 	defer client.mutex.Unlock()
 
 	if !client.stopped {
-		defer func() {
-			client.Close()
-		}()
 		client.stopped = true
 		close(client.stop)
 	}
@@ -123,6 +120,9 @@ func (client *MessageClient) Start(pipe *app.MessagePipe) {
 	}
 
 	err := upgrader.Upgrade(ctx, func(ws *websocket.Conn) {
+		defer func() {
+			client.Close()
+		}()
 		defer ws.Close()
 
 		var kontinue bool = true
@@ -143,7 +143,6 @@ func (client *MessageClient) Start(pipe *app.MessagePipe) {
 					Format: app.CLOSE_MESSAGE,
 					Body:   p,
 				}
-				fmt.Println("[debug]:: sending close.....")
 				pipe.Forward(client, message)
 				kontinue = false
 				continue
